2673e937e6bbddcb10947720b6f9c226f40f4803
gavalian <gavalian@meson.jlab.org>
2019-04-15 17:10:28 -0400

    modified draw groovy script to export 3D histograms to NRRD format

--- partons/run_docker.sh
+++ partons/run_docker.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-docker run  -v /Users/gavalian/Work/Software/project-6a.0.0/Femtography/codes:/root/workspace/codes -it pawelsznajder/partons-test
+docker run  -v /Users/gavalian/Work/Software/project-6a.0.0/Distribution/femtography/partons/codes:/root/workspace/codes -it pawelsznajder/partons-test
